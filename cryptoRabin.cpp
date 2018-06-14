92bae019efa5d58de3af650dc7ed1b394c96c2fb
v.riznyk <v.riznyk@samsung.com>
2018-06-14 12:01:31 +0300

    structure and out paramrters

--- cryptoRabin.cpp
+++ cryptoRabin.cpp
@@ -3,40 +3,52 @@
 #include <cryptopp/rsa.h>
 #include <cryptopp/rabin.h>
 #include <cryptopp/filters.h>
+#include <cryptopp/files.h>
  
 using namespace CryptoPP;
 using namespace std;
  
-int main(){
-AutoSeededRandomPool prng;
+int main()
+{
+    AutoSeededRandomPool prng;
 
-Rabin::PrivateKey privKey;
-privKey.GenerateRandomWithKeySize(prng, 128);
-Rabin::PublicKey pubKey(privKey);
+    Rabin::PrivateKey privKey;
+    privKey.GenerateRandomWithKeySize(prng, 128);
+    Rabin::PublicKey pubKey(privKey);
 
-string message, recovered;
-Integer m, c, r;
+    {
+        FileSink output("rsaprivate.dat");
+        privKey.DEREncode(output);
+    }
+    {
+        FileSink output("rsapublic.dat");
+        pubKey.DEREncode(output);
+    }
+    string message, recovered;
+    Integer m, c, r;
 
-message = "secret";
-cout << "message: " << message << endl;
+    message = "secret";
+    cout << "message: " << message << endl;
 
-// Treat the message as a big endian array
-m = Integer((const byte *)message.data(), message.size());
-cout << "m: " << hex << m << endl;
+    // Treat the message as a big endian array
+    m = Integer((const byte *)message.data(), message.size());
+    cout << "m: " << hex << m << endl;
 
-// Encrypt
-c = pubKey.ApplyFunction(m);
-cout << "c: " << hex << c << endl;
+    // Encrypt
+    c = pubKey.ApplyFunction(m);
+    cout << "c: " << hex << c << endl;
 
-// Decrypt
-r = privKey.CalculateInverse(prng, c);
-cout << "r: " << hex << r << endl;
+    // Decrypt
+    r = privKey.CalculateInverse(prng, c);
+    cout << "r: " << hex << r << endl;
 
-// Round trip it
-size_t req = r.MinEncodedSize();
-recovered.resize(req);
-r.Encode((byte *) &recovered[0], recovered.size());
+    // Round trip it
+    size_t req = r.MinEncodedSize();
+    recovered.resize(req);
+    r.Encode((byte *) &recovered[0], recovered.size());
 
-cout << "recovered: " << recovered << endl;	
+    cout << "recovered: " << recovered << endl;	
+
+    return 0;
 }
 
